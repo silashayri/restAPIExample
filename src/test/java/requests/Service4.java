6ca67efb3a891fd8435760579e414a29f630d629
silashayri <Sls15srmn>
2019-10-02 16:27:39 +0300

    update service 4 class..

--- src/test/java/requests/Service4.java
+++ src/test/java/requests/Service4.java
@@ -15,68 +15,68 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Service4 {
+    Properties prop = new Properties();
+
 
     @BeforeTest
     public void getData() throws IOException {
 
-        Properties prop = new Properties();
         FileInputStream fis = new FileInputStream("//Users//sahabt//Desktop//restApi//restAPIExample//src//files//env.properties");
         prop.load(fis);
 
 
-        prop.get("Host");
+        prop.getProperty("Host");
 
 
     }
 
 
     @Test
-    public void AddandDeletePlace(){
-String resbody = "{"+
-        "\"location\": {"+
-        "\"lat\": -33.8669710,"+
-        "\"lng\": 151.1958750"+
-        "},"+
-        "\"accuracy\": 50,"+
-        "\"name\": \"Google Shoes!\","+
-        "\"phone_number\": \"(02) 9374 4000\","+
-        "\"address\": \"48 Pirrama Road, Pyrmont, NSW 2009, Australia\","+
-        "\"types\": [\"shoe_store\"],"+
-        "\"website\": \"http://www.google.com.au/\","+
-        "\"language\": \"en-AU\""+
-        "}";
+    public void AddandDeletePlace() {
+        String resbody = "{" +
+                "\"location\": {" +
+                "\"lat\": -33.8669710," +
+                "\"lng\": 151.1958750" +
+                "}," +
+                "\"accuracy\": 50," +
+                "\"name\": \"Google Shoes!\"," +
+                "\"phone_number\": \"(02) 9374 4000\"," +
+                "\"address\": \"48 Pirrama Road, Pyrmont, NSW 2009, Australia\"," +
+                "\"types\": [\"shoe_store\"]," +
+                "\"website\": \"http://www.google.com.au/\"," +
+                "\"language\": \"en-AU\"" +
+                "}";
 
-    RestAssured.baseURI="http://216.10.245.166";
-    Response res = given().
-            queryParam("key","qaclick123").
-            body(resbody).
-            when().
-            post("/maps/api/place/add/json").
-            then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
-            body("status",equalTo("OK")).
-    extract().response();
+        RestAssured.baseURI = prop.getProperty("Host");
+        Response res = given().
+                queryParam("key", "qaclick123").
+                body(resbody).
+                when().
+                post("/maps/api/place/add/json").
+                then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
+                body("status", equalTo("OK")).
+                extract().response();
         /*************           Grab the Place ID from the Response             *********/
         String responseString = res.prettyPrint();
-   // System.out.println(responseString);
-    JsonPath jsp = new JsonPath(responseString);
-    String PlaceID = jsp.get("place_id");
-    System.out.println("Place ID: "+PlaceID);
+        // System.out.println(responseString);
+        JsonPath jsp = new JsonPath(responseString);
+        String PlaceID = jsp.get("place_id");
+        System.out.println("Place ID: " + PlaceID);
 
         /*************           Place this Place ID in the delete request             *********/
 
 
-    given().
-            queryParam("key","qaclick123").
-            body("{"+
+        given().
+                queryParam("key", "qaclick123").
+                body("{" +
 
-                   "\"place_id\": \""+PlaceID+"\" "+
-                "}").
-            when().
-            post("/maps/api/place/delete/json").
-            then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
-            body("status",equalTo("OK"));
-}
-
+                        "\"place_id\": \"" + PlaceID + "\" " +
+                        "}").
+                when().
+                post("/maps/api/place/delete/json").
+                then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and().
+                body("status", equalTo("OK"));
+    }
 
 
 }
